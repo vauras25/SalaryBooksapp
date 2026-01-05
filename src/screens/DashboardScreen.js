649554a6506a0d732d3f4fa366fa2415fbd3fcf5
vauras25 <growth@vauras.in>
2026-01-05 12:27:05 +0530

    dashboard

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -30,15 +30,13 @@ const Dashboard = () => {
   const [empData, setEmpData] = useState(null);
   const [imageUrl, setImageUrl] = useState(null);
   const [attendenceImageUrl, setAttendenceImageUrl] = useState(null);
-
+  const [rights, setRights] = useState(null);
 
   const loadToken = async () => {
     const t = await AsyncStorage.getItem("authToken");
     setToken(t);
     console.log("TOKEN LOADED:", t);
   };
-
-
 
   const fetchemployeedata = async () => {
     if (!token) return;
@@ -64,14 +62,19 @@ const Dashboard = () => {
       // }
       if (res.data?.status === "success") {
         const employeeData = res.data.employee_data;
-        console.log("employeeData",employeeData,"API_BASE_URL",API_BASE_URL);
-        
+        console.log("employeeData", employeeData, "API_BASE_URL", API_BASE_URL);
+
         setEmpData(employeeData);
 
         const profilePic = employeeData?.[0]?.profile_pic;
         saveImageUrl(profilePic);
         const attendencePic = employeeData?.[0]?.attendence_pic;
         saveAttendenceImageUrl(attendencePic);
+
+        const rightsData =employeeData?.[0]?.employee_details?.employment_hr_details?.emp_role_data?.rights;
+        console.log("rightsData",rightsData);
+        
+        setRights(rightsData);
       }
 
     } catch (error) {
@@ -97,7 +100,7 @@ const Dashboard = () => {
       ? `${API_BASE_URL}${profilePic.replace(/\\/g, "/")}`
       : null;
 
-    setImageUrl(url); 
+    setImageUrl(url);
     await AsyncStorage.setItem("imageUrl", url);
   };
   const saveAttendenceImageUrl = async (profilePic) => {
@@ -105,21 +108,17 @@ const Dashboard = () => {
       ? `${API_BASE_URL}${profilePic.replace(/\\/g, "/")}`
       : null;
 
-    setAttendenceImageUrl(url); 
+    setAttendenceImageUrl(url);
     await AsyncStorage.setItem("attendenceimageUrl", url);
   };
 
 
   useEffect(() => {
-    // const backAction = () => {
-    //   BackHandler.exitApp();
-    //   return true;
-    // };
     const loadData = async () => {
       try {
         const userData = JSON.parse(await AsyncStorage.getItem("userData"));
         setUserData(userData);
-        console.log(userData, "userData1234",API_BASE_URL);
+        console.log(userData, "userData1234", API_BASE_URL);
       } catch (error) {
         console.log("Error loading userData:", error);
       }
@@ -129,11 +128,6 @@ const Dashboard = () => {
     loadToken();
 
     fetchemployeedata();
-    // const backHandler = BackHandler.addEventListener(
-    //   "hardwareBackPress",
-    //   backAction
-    // );
-    // return () => backHandler.remove();
   }, [token, imageUrl]);
 
 
@@ -152,10 +146,6 @@ const Dashboard = () => {
         <ScrollView style={styles.container} contentContainerStyle={{ paddingBottom: 50 }}>
           {/* Header */}
           <View style={styles.headerRow}>
-            {/* <Image
-              source={require('${empData.profile_pic}')}
-              style={styles.profileImage}
-            /> */}
             <Image
               source={
                 imageUrl
@@ -176,13 +166,13 @@ const Dashboard = () => {
 
 
             <View style={styles.calendarSection}>
-              {/* Current Month */}
+
               <Text style={styles.monthText}>
                 {new Date().toLocaleString("default", { month: "long" })}{" "}
                 {new Date().getFullYear()}
               </Text>
 
-              {/* Calendar Grid */}
+
               <View style={styles.calendarGrid}>
                 {daysOfWeek.map((d, i) => (
                   <Text key={i} style={styles.dayHeader}>
@@ -195,7 +185,7 @@ const Dashboard = () => {
                   const year = now.getFullYear();
                   const month = now.getMonth();
                   const totalDays = new Date(year, month + 1, 0).getDate();
-                  const firstDay = new Date(year, month, 1).getDay(); // Sunday=0
+                  const firstDay = new Date(year, month, 1).getDay();
 
                   const calendarCells = [];
 
@@ -274,22 +264,17 @@ const Dashboard = () => {
                     showsText={false} // hide default text
                   />
 
-                  {/* Custom text overlay */}
                   <Text style={{ position: "absolute", color: "#ffffff", fontSize: 14, fontWeight: "600" }}>
                     88%
                   </Text>
                 </View>
 
-
-                {/* Attendance Stats */}
                 <View style={styles.stats}>
                   <Text style={styles.present}>Present: 22</Text>
                   <Text style={styles.absent}>Absent: 02</Text>
                   <Text style={styles.late}>Late: 04</Text>
                 </View>
               </View>
-
-
 
               <TouchableOpacity style={styles.markBtn}>
                 <Text style={styles.markBtnText}>Mark Attendance</Text>
@@ -298,7 +283,7 @@ const Dashboard = () => {
           </LinearGradient>
 
           <Reimbursement />
-          <Advance />
+          <Advance rights={rights} />
 
         </ScrollView>
         <BottomNavigation />
@@ -307,206 +292,11 @@ const Dashboard = () => {
   );
 };
 
-
-// const styles = StyleSheet.create({
-
-
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#0B132B",
-//     padding: 5,
-//   },
-//   greeting: {
-//     color: "#fff",
-//     fontSize: 22,
-//     fontWeight: "600",
-//     marginBottom: 10,
-//   },
-//   card: {
-//     backgroundColor: "#1C2541",
-//     borderRadius: 20,
-//     padding: 15,
-//     marginBottom: 15,
-//     alignItems: "center",
-//   },
-//   sectionTitle: {
-//     color: "#fff",
-//     fontSize: 10,
-//     fontWeight: "500",
-//     alignSelf: "flex-start",
-//   },
-
-//   subText: {
-//     color: "#aaa",
-//     fontSize: 13,
-//     marginTop: 8,
-//   },
-//   advanceText: {
-//     flexDirection: "row",
-//     justifyContent: "space-between",
-//     width: "100%",
-//     paddingHorizontal: 10,
-//     marginTop: 10,
-//   },
-//   grid: {
-//     flexDirection: "row",
-//     flexWrap: "wrap",
-//     justifyContent: "space-between",
-//   },
-//   gridItem: {
-//     width: "48%",
-//     backgroundColor: "#3A506B",
-//     paddingVertical: 20,
-//     borderRadius: 15,
-//     alignItems: "center",
-//     marginBottom: 10,
-//   },
-//   gridText: {
-//     color: "#fff",
-//     fontWeight: "600",
-//   },
-
-
-
-//   monthText: {
-//     color: "#fff",
-//     fontSize: 14,
-//     fontWeight: "600",
-//     textAlign: "center",
-//     marginBottom: 8,
-//   },
-//   calendarGrid: {
-//     flexDirection: "row",
-//     flexWrap: "wrap",
-//     width: "100%",
-//     marginTop: 6,
-//   },
-
-
-
-//   dayBox: {
-//     width: "14%",
-//     height: 28,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     borderRadius: 6,
-//     marginVertical: 2,
-//   },
-
-//   dayText: {
-//     fontSize: 12,
-//     fontWeight: "600",
-//   },
-
-
-//   markBtn: {
-//     marginTop: 10,
-//     backgroundColor: "#1E90FF",
-//     paddingHorizontal: 16,
-//     paddingVertical: 6,
-//     borderRadius: 10,
-//   },
-//   markBtnText: {
-//     color: "#fff",
-//     fontSize: 13,
-//     fontWeight: "600",
-//   },
-
-//   headerRow: {
-//     flexDirection: "row",       // make items appear in a row
-//     alignItems: "center",       // vertically center image & text
-//     marginBottom: 15,           // space below the header
-//   },
-
-//   profileImage: {
-//     width: 45,
-//     height: 45,
-//     borderRadius: 22.5,         // make it circular
-//     marginRight: 10,            // space between image and text
-//     borderWidth: 2,
-//     borderColor: "#1E90FF",     // optional border color
-//   },
-
-//   greeting: {
-//     color: "#fff",
-//     fontSize: 22,
-//     fontWeight: "600",
-//   },
-
-
-
-
-//   attendanceStats: {
-//     width: "33%",      // explicit width for stats
-//     alignItems: "center",
-//     justifyContent: "flex-start",
-//     paddingTop: 6,
-//     marginLeft: 20,
-
-//   },
-//   sectionTitle: {
-//     color: "#fff",
-//     fontSize: 16,
-//     fontWeight: "600",
-//     marginBottom: 10,
-//   },
-//   statsRow: {
-//     flexDirection: "row",
-//     alignItems: "center",
-//     justifyContent: "space-around",
-//     marginLeft: 25
-//   },
-//   stats: {
-//     marginLeft: 20,
-//   },
-//   present: {
-//     color: "#8fb8f5ff",
-//     fontSize: 14,
-//     marginBottom: 4,
-//   },
-//   absent: {
-//     color: "#F44336",
-//     fontSize: 14,
-//     marginBottom: 4,
-//   },
-//   late: {
-//     color: "#FFC107",
-//     fontSize: 14,
-//   },
-//   dayHeader: {
-//     color: "#A9A9A9",
-//     width: "14%",
-//     textAlign: "center",
-//     fontSize: 12,
-//     fontWeight: "600",
-//     marginVertical: 2,
-//   },
-
-
-//   attendanceCard: {
-//     flexDirection: "row",
-//     backgroundColor: "#1C2541",
-//     borderRadius: 20,
-//     padding: 15,
-//     marginBottom: 15,
-//     alignItems: 'flex-start',
-//   },
-
-//   calendarSection: {
-//     width: "45%",      // explicit width for calendar
-//     marginRight: 12,
-//   },
-
-
-// });
-
 const styles = StyleSheet.create({
 
 
   container: {
     flex: 1,
-    // backgroundColor: "#0B132B",
-    //  backgroundColor: 'rgb(11, 47, 73)',
     padding: 5,
   },
   greeting: {
@@ -522,13 +312,6 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     alignItems: "center",
   },
-  // sectionTitle: {
-  //   color: "#fff",
-  //   fontSize: 10,
-  //   fontWeight: "500",
-  //   alignSelf: "flex-start",
-  // },
-
   subText: {
     color: "#aaa",
     fontSize: 13,
@@ -559,65 +342,19 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
 
-
-
-  // monthText: {
-  //   color: "#fff",
-  //   fontSize: 14,
-  //   fontWeight: "600",
-  //   textAlign: "center",
-  //   marginBottom: 8,
-  // },
-  // calendarGrid: {
-  //   flexDirection: "row",
-  //   flexWrap: "wrap",
-  //   width: "100%",
-  //   marginTop: 6,
-  // },
-
-
-
-  // dayBox: {
-  //   width: "14%",
-  //   height: 28,
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   borderRadius: 6,
-  //   marginVertical: 2,
-  // },
-
-  // dayText: {
-  //   fontSize: 12,
-  //   fontWeight: "600",
-  // },
-
-
-  // markBtn: {
-  //   marginTop: 10,
-  //   backgroundColor: "#1E90FF",
-  //   paddingHorizontal: 16,
-  //   paddingVertical: 6,
-  //   borderRadius: 10,
-  // },
-  // markBtnText: {
-  //   color: "#fff",
-  //   fontSize: 13,
-  //   fontWeight: "600",
-  // },
-
   headerRow: {
-    flexDirection: "row",       // make items appear in a row
-    alignItems: "center",       // vertically center image & text
-    marginBottom: 15,           // space below the header
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 15,
   },
 
   profileImage: {
     width: 45,
     height: 45,
-    borderRadius: 22.5,         // make it circular
-    marginRight: 10,            // space between image and text
+    borderRadius: 22.5,
+    marginRight: 10,
     borderWidth: 2,
-    borderColor: "#1E90FF",     // optional border color
+    borderColor: "#1E90FF",
   },
 
   greeting: {
@@ -625,70 +362,6 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "600",
   },
-
-
-
-
-  // attendanceStats: {
-  //   width: "33%",      // explicit width for stats
-  //   alignItems: "center",
-  //   justifyContent: "flex-start",
-  //   paddingTop: 6,
-  //   marginLeft: 20,
-
-  // },
-  // sectionTitle: {
-  //   color: "#fff",
-  //   fontSize: 16,
-  //   fontWeight: "600",
-  //   marginBottom: 10,
-  // },
-  // statsRow: {
-  //   flexDirection: "row",
-  //   alignItems: "center",
-  //   justifyContent: "space-around",
-  //   marginLeft: 25
-  // },
-  // stats: {
-  //   marginLeft: 20,
-  // },
-  // present: {
-  //   color: "#8fb8f5ff",
-  //   fontSize: 14,
-  //   marginBottom: 4,
-  // },
-  // absent: {
-  //   color: "#F44336",
-  //   fontSize: 14,
-  //   marginBottom: 4,
-  // },
-  // late: {
-  //   color: "#FFC107",
-  //   fontSize: 14,
-  // },
-  // dayHeader: {
-  //   color: "#A9A9A9",
-  //   width: "14%",
-  //   textAlign: "center",
-  //   fontSize: 12,
-  //   fontWeight: "600",
-  //   marginVertical: 2,
-  // },
-
-
-  // attendanceCard: {
-  //   flexDirection: "row",
-  //   backgroundColor: "#1C2541",
-  //   borderRadius: 20,
-  //   padding: 15,
-  //   marginBottom: 15,
-  //   alignItems: 'flex-start',
-  // },
-
-  // calendarSection: {
-  //   width: "45%",      // explicit width for calendar
-  //   marginRight: 12,
-  // },
 
   attendanceCard: {
     flexDirection: "row",
@@ -699,21 +372,19 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     alignItems: 'flex-start',
     gap: 16,
-    height:170
+    height: 170
   },
 
   calendarSection: {
-    // flex: 1,
     width: '50%',
   },
 
   monthText: {
     color: "#fff",
-    // fontSize: 15,
     fontWeight: "600",
     textAlign: "center",
     marginBottom: 10,
-    marginTop:5
+    marginTop: 5
   },
 
   calendarGrid: {
@@ -739,24 +410,17 @@ const styles = StyleSheet.create({
     borderRadius: 3,
     marginBottom: 2,
   },
-
   dayText: {
     fontSize: 9,
     fontWeight: "500",
   },
-
-
-
-
-
-
   attendanceStats: {
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-start",
     paddingTop: 0,
     width: "50%",
-    marginTop:5
+    marginTop: 5
   },
 
   sectionTitle: {
@@ -766,7 +430,6 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     textAlign: "left",
     letterSpacing: 0.3,
-    // marginLeft:40
   },
 
   statsRow: {
@@ -818,7 +481,6 @@ const styles = StyleSheet.create({
     borderRadius: 18,
     alignItems: "center",
     marginTop: 0,
-    // marginLeft:25
   },
 
   markBtnText: {
@@ -828,6 +490,4 @@ const styles = StyleSheet.create({
   },
 
 });
-
-
 export default Dashboard;
